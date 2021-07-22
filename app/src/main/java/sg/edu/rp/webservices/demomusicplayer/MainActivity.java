481d40672a78a1822fb5ef2826ee56ca4ef869e0
19041582-Qayyum-Danish-Tan <19041582@myrp.edu.sg>
2021-07-22 12:43:25 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/demomusicplayer/MainActivity.java
@@ -0,0 +1,50 @@
+package sg.edu.rp.webservices.demomusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
+
+        int permissionCheck = PermissionChecker.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permissionCheck != PermissionChecker.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+            Log.i("permission", "Permission not granted");
+            return;
+        }
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // starting the service
+                startService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stopping the service
+                stopService(new Intent(MainActivity.this, MyService.class));
+            }
+        });
+
+    }
+
+}
